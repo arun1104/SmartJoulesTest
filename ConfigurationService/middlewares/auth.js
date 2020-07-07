9ffc436d231b2e8ea6e786bea7b8d09a2f1ae087
unknown <arunrajeevan1104@gmail.com>
2020-07-07 13:04:59 +0530

    added auth middleware

--- ConfigurationService/middlewares/auth.js
+++ ConfigurationService/middlewares/auth.js
@@ -6,24 +6,10 @@ const filePath = path.join(__dirname, 'private');
 const jwt = require('jsonwebtoken');
 module.exports.authMiddleware = function(req, res, next) {
   try {
-    if (req.path.toLowerCase().includes('/files')){
-      if (req.method === 'POST'){
-        let cert = fs.readFileSync(filePath);
-        let decoded = jwt.verify(req.headers.authorization, cert, { algorithms: ['RS256'] });
-        console.log(decoded);
-        next();
-      } else {
-        next();
-      }
-    } else if (req.swagger.security.length > 0){
-      let cert = fs.readFileSync(filePath);
-      let decoded = jwt.verify(req.headers.authorization, cert, { algorithms: ['RS256'] });
-      console.log(decoded);
-      next();
-    } else {
-      next();
-    }
-
+    let cert = fs.readFileSync(filePath);
+    let decoded = jwt.verify(req.headers.authorization, cert, { algorithms: ['RS256'] });
+    console.log(decoded);
+    next();
   } catch (err) {
     res.status(400).send({message: 'Invalid token'});
   }
